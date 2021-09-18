ee2075b0a1381ca05d5e6ecd7f1d71fd37f7adcb
Jacob Hilker <jacob.hilker2@gmail.com>
2021-09-18 10:58:31 -0400

    finished base theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,111 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['layouts/**/*.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    gridTemplateAreas: {
+      mobile: ['header header header', 'main main main', 'footer footer footer'],
+      desktop: ['sidebar header header', 'sidebar main main', 'sidebar footer footer'],
+    },
+    extend: {
+        gridTemplateColumns: {
+        layout: '0.7fr 2.3fr 1fr',
+      },
+      gridTemplateRows: {
+        layout: '0.2fr 2.6fr 0.2fr',
+      },
+        fontFamily: {
+            header: "'Roboto Slab', serif",
+        },
+
+        height: {
+          '13': '52px',
+          '18': '66px'
+        },
+
+        colors: {
+          blueGray: colors.blueGray
+        },
+        typography: (theme) => ({
+        dark: {
+          css: [
+            {
+              color: theme('colors.gray.300'),
+              '[class~="lead"]': {
+                color: theme('colors.gray.300'),
+              },
+              a: {
+                color: theme('colors.white'),
+              },
+              strong: {
+                color: theme('colors.white'),
+              },
+              'ol > li::before': {
+                color: theme('colors.gray.400'),
+              },
+              'ul > li::before': {
+                backgroundColor: theme('colors.gray.600'),
+              },
+              hr: {
+                borderColor: theme('colors.gray.200'),
+              },
+              blockquote: {
+                color: theme('colors.gray.200'),
+                borderLeftColor: theme('colors.gray.600'),
+              },
+              h1: {
+                color: theme('colors.white'),
+              },
+              h2: {
+                color: theme('colors.white'),
+              },
+              h3: {
+                color: theme('colors.white'),
+              },
+              h4: {
+                color: theme('colors.white'),
+              },
+              'figure figcaption': {
+                color: theme('colors.gray.400'),
+              },
+              code: {
+                color: theme('colors.white'),
+              },
+              'a code': {
+                color: theme('colors.white'),
+              },
+              pre: {
+                color: theme('colors.gray.200'),
+                backgroundColor: theme('colors.gray.800'),
+              },
+              thead: {
+                color: theme('colors.white'),
+                borderBottomColor: theme('colors.gray.400'),
+              },
+              'tbody tr': {
+                borderBottomColor: theme('colors.gray.600'),
+              },
+            },
+          ],
+        },
+      }),
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+ variants: {
+   gridTemplateAreas: ['responsive'],
+   scrollbar: ['dark'],
+   extend: {
+     typography: ['dark'],
+   },
+ },
+
+  plugins: [
+    require('@savvywombat/tailwindcss-grid-areas'),
+    require('tailwind-scrollbar'),
+    require('@tailwindcss/typography')({
+        className: 'markdown',
+    }),
+  ],
 }
